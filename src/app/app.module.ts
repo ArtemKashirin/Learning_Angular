a53d4035526ac211aa2455d1e9896a0de5402e9e
artem_kashirin <artem.kashirin1986@gmail.com>
2021-07-17 19:46:07 +0300

    Section: Life cycle hooks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HooksComponent } from './hooks/hooks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HooksComponent
   ],
   imports: [
     BrowserModule
