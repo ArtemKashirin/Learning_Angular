22046e3489074c0581237c2befe63ab22576cf63
artem_kashirin <artem.kashirin1986@gmail.com>
2021-07-19 13:54:30 +0300

    Hooks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HooksComponent } from './hooks/hooks.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HooksComponent
+    HooksComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hooks/hooks.component.ts
+++ src/app/hooks/hooks.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  SimpleChanges,
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges, OnDestroy,
+  OnInit, Input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-hooks',
   templateUrl: './hooks.component.html',
   styleUrls: ['./hooks.component.scss']
 })
-export class HooksComponent implements OnInit {
+export class HooksComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  public hooks: string = 'https://angular.io/guide/lifecycle-hooks';
+  @Input() value: string = '';
 
-  constructor() { }
+  // public value: string = 'Artem';
+
+
+  constructor() {
+    console.log('constructor!')
+  }
+
+//Последовательность событий жизненного цикла
+  ngOnChanges(changes: SimpleChanges): void {
+    // Отвечает, когда Angular УСТАНАВЛИВАЕТ ИЛИ СБРАСЫВАЕТ СВОЙСТВО ВВОДА С ПРИВЯЗКОЙ К ДАННЫМ. Метод получает
+    // SimpleChanges объект текущего и предыдущего значений свойств.
+    console.log('ngOnChanges', changes)
+  }
 
   ngOnInit(): void {
+    // Инициализируйте директиву или компонент после того, как Angular сначала отобразит свойства с привязкой к
+    // данным и задаст входные свойства директивы или компонента.
+    // Вызывается один раз, после первого ngOnChanges()
+    console.log('ngOnInit')
+  }
+
+  ngDoCheck(): void {
+    // Обнаруживает и действует в соответствии с изменениями, которые Angular не может или не хочет обнаружить
+    // самостоятельно.
+    // Вызывается сразу после ngOnChanges() каждого запуска обнаружения изменений и сразу после ngOnInit() первого
+    // запуска.
+    console.log('ngDoCheck')
+  }
+
+  ngAfterContentInit(): void {
+    // Отвечайте после того, как Angular проецирует внешний контент в представление компонента или в представление,
+    // в котором находится директива.
+    // Вызывается один раз после первого ngDoCheck()
+    console.log('ngAfterContentInit')
+  }
+
+  ngAfterContentChecked(): void {
+    // Ответить после того, как Angular проверит содержимое, проецируемое в директиву или компонент.
+    // Вызывается после ngAfterContentInit()и каждый последующий ngDoCheck()
+    console.log('ngAfterContentChecked')
+
+  }
+
+  ngAfterViewInit(): void {
+    // Ответить после того, как Angular инициализирует представления компонента и дочерние представления или
+    // представление, содержащее директиву.
+    // Вызывается один раз после первого ngAfterContentChecked()
+    console.log('ngAfterViewInit')
+
+  }
+
+  ngAfterViewChecked(): void {
+    // Ответить после того, как Angular проверит представления компонента и дочерние представления или
+    // представление, содержащее директиву.
+    // Вызывается после ngAfterViewInit()и каждого последующего ngAfterContentChecked()
+    console.log('ngAfterViewChecked')
+
+  }
+
+  ngOnDestroy(): void {
+    // Вызывается непосредственно перед тем, как Angular уничтожит директиву или компонент.
+    // Когда переходим по ссылке в другое место приложения
+    console.log(' ngOnDestroy')
   }
 
 }
